ab1ade981d196a5c0cc5018099394fbae7d64fe4
pmquinn2473 <pmquinn24@yahoo.com>
2024-05-24 23:04:52 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "1048255308264525824"; // your league ID
+export const leagueID = "1048365975986483200"; // your league ID
 export const leagueName = "Phillips, Phamily, & Phriends"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
